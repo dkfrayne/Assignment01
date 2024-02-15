8e8a2ad08c29339e09a668156928c4caa0d06b8c
dkfrayne <dkfrayne@gmail.com>
2024-02-14 22:25:20 -0800

    completed project

---
+++ js/script.js
@@ -0,0 +1,137 @@
+// // STEP 1 ////////////////////////////////////////////////
+
+// let someMonth;
+// function theMonth() {}
+// let currentMonth;
+// let summerMonth;
+// let myLibraryFunction;
+
+// // STEP 2 ////////////////////////////////////////////////
+
+// // Numeric Literal:
+// 5
+// // String Literal:
+// '5'
+// // Boolean Literal
+// true
+// // Null Literal:
+// null
+
+// // STEP 3 ////////////////////////////////////////////////
+
+// // First example:
+// let x = 1 + 2;
+// // Second example:
+// let y = x + 3;
+
+// // STEP 4 ////////////////////////////////////////////////
+
+// let firstName, lastName, address, city, state,  zipCode, age, referralSource, mayContact;
+
+// // STEP 5 ////////////////////////////////////////////////
+
+// First example:
+// let firstName = "Denny";
+
+// // Second example:
+// let address, city, state;
+// state = 'CA';
+
+// // Third example:
+// let age = Number(prompt("What year is it?")).toFixed(0) - confirm("Press OK if September 14th of this year is in the future, and Cancel if it has passed.") - 1998;
+
+// // STEP 6 ////////////////////////////////////////////////
+
+// let myVariable = 2024 + "February";
+// console.log(myVariable);
+
+// // STEP 7 ////////////////////////////////////////////////
+
+// let firstVariable = true + "!";
+// document.write(`First Variable: ${firstVariable}<br/>`);
+// let secondVariable = false + 5;
+// document.write(`Second Variable: ${secondVariable}<br/>`);
+
+// // STEP 8 ////////////////////////////////////////////////
+
+// // A slash \ is needed to escape the apostrophe ' in the word: I'm
+// let someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."'
+// console.log(someString)
+
+// // STEP 9 ////////////////////////////////////////////////
+
+// let myNull = null;
+// console.log(myNull);
+
+// let myUndefined;
+// console.log(myUndefined);
+
+// // STEP 10 ////////////////////////////////////////////////
+
+// console.log(`String:  ${typeof('Hello, world!')}`);
+// console.log(`Number:  ${typeof(3.1415926535897932384626433832795028841971693993)}`);
+// console.log(`Boolean: ${typeof(true < false)}`);
+// console.log(`Object:  ${typeof(window)}`);
+// console.log(`undefined: ${typeof(undefined)}`);
+
+// // STEP 11 ////////////////////////////////////////////////
+
+// alert("Hello " + "Denny Frayne" + ", welcome to the JavaScript class!");
+
+// // STEP 12 ////////////////////////////////////////////////
+
+// let name = "Denny Frayne";
+// alert("Hello " + name + ", welcome to the JavaScript class!");
+
+// // STEP 13 ////////////////////////////////////////////////
+
+// let name = "Denny Frayne", course = "JavaScript";
+// alert("Hello " + name + ", welcome to the " + course + " class!");
+
+// // STEP 14 ////////////////////////////////////////////////
+
+// let name = "Denny Frayne", course = "JavaScript";
+// alert("Hello " + name + ".\nWelcome to the " + course + " class!");
+
+// // STEP 15 ////////////////////////////////////////////////
+
+// let name = prompt("Enter your name below:");
+
+// // STEP 16 ////////////////////////////////////////////////
+
+// let course = prompt("Enter the name of the class you are taking below:");
+
+// // STEP 17 ////////////////////////////////////////////////
+
+// let x = 10;
+// let y = 20;
+// console.log(x + y);
+
+// // STEP 18 ////////////////////////////////////////////////
+
+// let x = 20;
+// x += 20;
+// console.log(x);
+
+// // STEP 19 ////////////////////////////////////////////////
+
+// let x = 20;
+// x *= 5;
+// console.log(x);
+
+// // STEP 20 ////////////////////////////////////////////////
+
+// let x = 20 % 3;
+// x /= 1;
+// console.log(x);
+
+// // STEP 21 ////////////////////////////////////////////////
+
+// let x = 25;
+// console.log((x % 10 < 5) || (x / 10 < 5));
+
+// // STEP 22 ////////////////////////////////////////////////
+
+// let y = -5;
+// console.log(y*y > 10 && y > 0);
+
